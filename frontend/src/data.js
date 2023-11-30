525bbdd78808cd31d6cf8219092518662cface45
Dana Campan <campan.dana15@gmail.com>
2023-11-30 20:16:07 +0200

    Implement HomeScreen, ProductScreen, server.js

---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+const data = {
+  products: [
+    {
+      name: 'Cana pictata manual',
+      slug: 'cana-pictata-manual',
+      category: 'Ustensile bucatarie',
+      image: './images/p1.jpg',
+      price: 40,
+      countInStock: 15,
+      commercializer: 'ObiectePictate',
+      rating: 4.0,
+      numReviews: 5,
+      description: 'Cana pictata manual cu modele deosebite',
+    },
+    {
+      name: 'Covor albastru deschis',
+      slug: 'covor-albastru-deschis',
+      category: 'Decoratiuni de casa',
+      image: './images/p2.jpg',
+      price: 100,
+      countInStock: 10,
+      commercializer: 'Atelierul Oanei',
+      rating: 4.5,
+      numReviews: 20,
+      description:
+        'Covor albastru deschis, cu un model inspirat din stilul rustic',
+    },
+    {
+      name: 'Cercei cu perle',
+      slug: 'cercei-cu-perle',
+      category: 'Bijuterii',
+      image: './images/p3.jpg',
+      price: 70,
+      countInStock: 30,
+      commercializer: 'Bijuterii HandMade',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'Cercei eleganti cu perle din aur, perfecti pentru orice ocazie',
+    },
+    {
+      name: 'Lănțișor din aur cu pandantiv roșu',
+      slug: 'lantisor-cu-pandantiv',
+      category: 'Bijuterii',
+      image: './images/p4.jpg',
+      price: 120,
+      countInStock: 20,
+      commercializer: 'Teilor',
+      rating: 4.6,
+      numReviews: 5,
+      description:
+        'Lanțul elegant din aur este însoțit de un pandantiv distins în nuanțe vibrante de roșu, aducând o notă de rafinament și căldură la orice ținută.',
+    },
+  ],
+};
+export default data;
