49954533570badd27dbbcf574556321467d322a5
Dana Campan <campan.dana15@gmail.com>
2023-11-30 17:12:41 +0200

    Am creat aplicatia

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Arti Market</a>
+      </header>
+      <main>Pune produsele</main>
+    </div>
+  );
+}
+
+export default App;
